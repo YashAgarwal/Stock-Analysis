789aa7378c96c55400c9f89b22fb870c71ea841d
Yash Agarwal <agrwal.ysh94@gmail.com>
2017-10-24 22:55:46 +0530

    creating repo

---
+++ main.py
@@ -0,0 +1,48 @@
+import csv
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dates = []
+prices = []
+
+# dataFileName = ''
+# data = pd.read_csv(dataFileName)
+
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader  = csv.reader(csvfile)
+        next(csvFileReader)
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[1]))
+    return
+
+def predict_prices(dates, prices, x):
+    dates = np.reshape(dates, (len(dates), 1))
+    # a linear support vector regression
+
+    svr_lin = SVR(kernel = 'linear', C=1e3)
+    svr_poly = SVR(kernel = 'poly', C=1e3, degree = 2)
+    svr_rbf = SVR(kernel = 'rbf', C=1e3, gamma = 0.1)
+    svr_lin.fit(dates, prices)
+    svr_poly.fit(dates, prices)
+    svr_rbf.fit(dates, prices)
+
+    plt.scatter(dates, prices, color = 'black', label = 'Data')
+    plt.plot(dates, svr_rbf.predict(dates), color = 'red', label = 'RBF Model')
+    plt.plot(dates, svr_lin.predict(dates), color = 'green', label = 'Linear Model')
+    plt.plot(dates, svr_poly.predict(dates), color = 'blue', label = 'Polynomial Model')
+    plt.xlabel('Dates')
+    plt.ylabel('Prices')
+    plt.title('Support Vector Regression')
+    plt.legend()
+    plt.show()
+
+    return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+
+get_data('tsla.csv')
+predicted_price = predict_prices(dates, prices, 29)
+
+print(predicted_price)
